e06b1b4af0973e4b0ce236b8a53b7b8fa24e77c0
Raymond Choi <tmd0228@gmail.com>
2026-04-30 16:17:23 +0800

    add CreateGroup component and sending data to firebase

--- src/pages/CreateGroup/CreateGroup.jsx
+++ src/pages/CreateGroup/CreateGroup.jsx
@@ -1,29 +1,60 @@
 import React, { useState } from 'react'
 import styles from './CreateGroup.module.css'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../../firebaseConfig/firebase'
+import { auth } from '../../firebaseConfig/firebase'
+import { useNavigate } from 'react-router-dom'
 
 function CreateGroup() {
 
     const [groupName, setGroupName] = useState('')
     const [description, setDescription] = useState('')
 
+    const navigate = useNavigate()
 
-    const handleCreateGroup = () => {
-        //groupName, ownerId, createdAt, description, isPrivate
+    //groupName, ownerId, createdAt, description, isPrivate
+    async function handleCreateGroup(e) {
+        e.preventDefault()
+        try {
+            const docRef = collection(db, 'groups')
+            const docData = await addDoc(docRef, {
+                groupName,
+                description,
+                createdAt: serverTimestamp(),
+                ownerId: auth.currentUser.uid,
+                isPrivate: true
+            })
+
+            const membershipRef = collection(db, 'membership')
+            const membershipData = await addDoc(membershipRef, {
+                groupId: docData.id,
+                member: auth.currentUser.uid
+            })
+
+            navigate('/')
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-  return (
-    <div>
-        <form onSubmit={handleCreateGroup}>
-            <label htmlFor="groupName">그룹이름</label>
-            <input type="text" id='groupName' value={groupName} onChange={(e)=> setGroupName(e.target.value)} required />
-            
-            <label htmlFor="description">설명</label>
-            <input type="textarea" id='description' value={description} onChange={(e)=> setDescription(e.target.value)} required />
+    return (
+        <div>
+            <form onSubmit={handleCreateGroup}>
+                <label htmlFor="groupName">그룹이름</label>
+                <input type="text" id='groupName' value={groupName} onChange={(e) => setGroupName(e.target.value)} required />
 
-            <button type='submit'>그룹만들기</button>
-        </form>
-    </div>
-  )
+                <label htmlFor="description">설명</label>
+                <textarea
+                    id='description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                />
+
+                <button type='submit'>그룹만들기</button>
+            </form>
+        </div>
+    )
 }
 
 export default CreateGroup
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebaseConfig/firebase'
+import styles from './Home.module.css'
 
 function Home() {
 
@@ -18,7 +19,7 @@ function Home() {
   }, [])
 
   return (<div>
-    {user && <div>
+    {user && <div className={styles.buttons}>
       <Link className='primaryButton' to='/joinGroup'>Join Group</Link>
       <Link className='primaryButton' to='/createGroup'>Create Group</Link>
       <Link className='primaryButton' to='/test'>test</Link>
