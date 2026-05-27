1f46f863f9f6e3dd93a06ce28452aa6b5f985b28
Raymond Choi <tmd0228@gmail.com>
2026-05-27 14:34:29 +0800

    adjust css styling

--- src/pages/GroupDashboard/GroupDashboard.jsx
+++ src/pages/GroupDashboard/GroupDashboard.jsx
@@ -1,13 +1,19 @@
 import styles from './GroupDashboard.module.css'
+import { Link } from 'react-router-dom'
 
 function GroupDashboard() {
   return (
     <div className={styles.body}>
-        <div className={styles.leftSidebar}>
-            <button className='primaryButton'>+그룹만들기</button>
+      <div className={styles.leftSidebar}>
+        <div className={styles.leftTop}>
+          <Link to='/createGroup' className='primaryButton'>+그룹만들기</Link>
         </div>
-        <div className={styles.main}></div>
-        <div className={styles.rightSidebar}></div>
+        <div className={styles.leftBottom}>
+          <p>내 그룹</p>
+        </div>
+      </div>
+      <div className={styles.main}></div>
+      <div className={styles.rightSidebar}></div>
     </div>
   )
 }
